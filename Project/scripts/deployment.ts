2a570af974c6bbe80da01215052eeeb4caf632a8
Morgan Weaver <morganjweaver@gmail.com>
2022-07-24 10:55:07 +0200

    deploy script for token and ballot

---
+++ Project/scripts/deployment.ts
@@ -0,0 +1,83 @@
+import { ethers } from "ethers"; // Hardhat for testing
+import "dotenv/config";
+import * as ballotJson from "../artifacts/contracts/CustomBallot.sol/CustomBallot.json";
+import * as tokenJson from "../artifacts/contracts/Token.sol/MyToken.json";
+import { CustomBallot, MyToken } from "../typechain";
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+// Do never expose your keys like this
+
+// DEPLOYS TO GOERLI, NOT HARDHAT SIM!!
+
+const EXPOSED_KEY =
+  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+
+function convertStringArrayToBytes32(array: string[]) {
+  const bytes32Array = [];
+  for (let index = 0; index < array.length; index++) {
+    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+  }
+  return bytes32Array;
+}
+async function checkBalance(signer: ethers.Wallet) {
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  console.log(`Wallet balance ${balance}`);
+  if (balance < 0.01) {
+    console.log("INSUFFICIENT ETH BALANCE");
+    return false;
+  }
+  return true;
+}
+async function main() {
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  console.log(`Using address ${wallet.address}`);
+  const provider = ethers.providers.getDefaultProvider("goerli");
+  const signer = wallet.connect(provider);
+  console.log("Proposals: ");
+  const proposals = process.argv.slice(2);
+  if (proposals.length < 2) throw new Error("Not enough proposals provided");
+  proposals.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+  });
+  console.log("Deploying token");
+  if (!checkBalance(signer)) {
+    return;
+  }
+  const tokenFactory = new ethers.ContractFactory(
+    tokenJson.abi,
+    tokenJson.bytecode,
+    signer
+  );
+  const tokenContract = await tokenFactory.deploy();
+  console.log("Awaiting confirmations");
+  await tokenContract.deployed();
+  console.log("Completed");
+  console.log(`Token contract deployed at ${tokenContract.address}`);
+  // BALLOT CONTRACT
+  if (!checkBalance(signer)) {
+    return;
+  }
+  console.log("Deploying Ballot contract");
+  const ballotFactory = new ethers.ContractFactory(
+    ballotJson.abi,
+    ballotJson.bytecode,
+    signer
+  );
+  const ballotContract = await ballotFactory.deploy(
+    convertStringArrayToBytes32(proposals),
+    tokenContract.address
+  );
+  console.log("Awaiting confirmations");
+  await ballotContract.deployed();
+  console.log("Completed");
+  console.log(`Ballot contract deployed at ${ballotContract.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
