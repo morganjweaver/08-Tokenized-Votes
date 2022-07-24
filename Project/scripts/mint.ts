bacb2b2df0849fbe75bf22bb905a05961fc5c3dc
Hrvoje <stjepan.hrvoje@gmail.com>
2022-07-24 19:39:58 +0200

    Common functions extracted into single helper

--- Project/scripts/mint.ts
+++ Project/scripts/mint.ts
@@ -2,42 +2,12 @@ import { Contract, ethers } from "ethers"; // Hardhat for testing
 import "dotenv/config";
 import * as tokenJson from "../artifacts/contracts/Token.sol/MyToken.json";
 import { MyToken } from "../typechain";
-
-// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
-// Do never expose your keys like this
-
-// DEPLOYS TO GOERLI, NOT HARDHAT SIM!!
+import { checkBalance, getSigner } from "../helpers/utils";
 
 const DEFAULT_VOTING_POWER = 10;
 
-const EXPOSED_KEY =
-  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
-
-function convertStringArrayToBytes32(array: string[]) {
-  const bytes32Array = [];
-  for (let index = 0; index < array.length; index++) {
-    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
-  }
-  return bytes32Array;
-}
-async function checkBalance(signer: ethers.Wallet) {
-  const balanceBN = await signer.getBalance();
-  const balance = Number(ethers.utils.formatEther(balanceBN));
-  console.log(`Wallet balance ${balance}`);
-  if (balance < 0.01) {
-    console.log("INSUFFICIENT ETH BALANCE");
-    return false;
-  }
-  return true;
-}
 async function main() {
-  const wallet =
-    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
-      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
-  console.log(`Using address ${wallet.address}`);
-  const provider = ethers.providers.getDefaultProvider("goerli");
-  const signer = wallet.connect(provider);
+  const signer = getSigner();
   if (!checkBalance(signer)) {
     return;
   }
