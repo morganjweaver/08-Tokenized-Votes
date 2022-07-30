38e5b389258873b9d6eeeb023c140b43c4995db4
Morgan Weaver <morganjweaver@gmail.com>
2022-07-30 16:20:23 +0100

    natspec addition

--- Project/contracts/CustomBallot.sol
+++ Project/contracts/CustomBallot.sol
@@ -5,6 +5,10 @@ interface IERC20Votes {
     function getPastVotes(address, uint256) external view returns (uint256);
 }
 
+/// @title CustomBallot Contract 
+/// @author Matheus Pagani
+/// @notice Allows creating proposals and voting based on ERC20 voting tokens with partner contract Token.sol
+/// @dev Implements voting-extended ERC20 to determine number of votes per user 
 contract CustomBallot {
     event Voted(
         address indexed voter,
@@ -32,6 +36,10 @@ contract CustomBallot {
         referenceBlock = block.number;
     }
 
+    /// @notice Allows user to vote on given proposal with amount of votes
+    /// @dev Emits Voted event on completion
+    /// @param proposal the number of the desired proposal to vote on, zero-indexed
+    /// @param amount whole number of votes to cast based on personal count of voting tokens
     function vote(uint256 proposal, uint256 amount) external {
         uint256 votingPowerAvailable = votingPower();
         require(votingPowerAvailable >= amount, "Has not enough voting power");
@@ -40,10 +48,14 @@ contract CustomBallot {
         emit Voted(msg.sender, proposal, amount, proposals[proposal].voteCount);
     }
 
+    /// @notice Returns name of proposal with most votes
+    /// @return winnerName_ byte32-encoded string value
     function winnerName() external view returns (bytes32 winnerName_) {
         winnerName_ = proposals[winningProposal()].name;
     }
 
+    /// @notice Returns index number of proposal with most votes
+    /// @return winningProposal_ Zero-indexed value of winning proposal number
     function winningProposal() public view returns (uint256 winningProposal_) {
         uint256 winningVoteCount = 0;
         for (uint256 p = 0; p < proposals.length; p++) {
@@ -54,12 +66,16 @@ contract CustomBallot {
         }
     }
 
+    /// @notice Returns remaining votes of the message sender.
+    /// @return votingPower_ number of total votes minus votes used for msg.sender
     function votingPower() public view returns (uint256 votingPower_) {
         votingPower_ =
             voteToken.getPastVotes(msg.sender, referenceBlock) -
             spentVotePower[msg.sender];
     }
 
+    /// @notice Returns count of proposals that have been created. 
+    /// @return numProposals length of proposals array
     function getProposalsLength() public view returns (uint256 numProposals) {
         return proposals.length;
     }
--- Project/contracts/Token.sol
+++ Project/contracts/Token.sol
@@ -6,19 +6,29 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
+/// @title A simple ERC20 voting token contract
+/// @author Matheus Pagani
+/// @notice Use this contract with CustomBallot.sol to mint voting tokens and vote
+/// @dev This contract implements several overrides required to make the interfaces work
+/// @custom:teaching This is a contract meant for teaching only
 contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
+    
+    /// @dev Token name and symbol are fixed.
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    /// @notice Mints new voting tokens
+    /// @param to address to send the tokens to
+    /// @param amount How many tokens to send
+    /// @notice Only the contract deployer/MINTER_ROLE can mint tokens
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    // The following functions are overrides required by Solidity.
+    /// @dev The following functions are overrides required by Solidity.
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
