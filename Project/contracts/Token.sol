dffe4933b6c8b70c7cbeefb9b3ad072c5130dbf1
Hrvoje <stjepan.hrvoje@gmail.com>
2022-07-30 16:55:31 +0200

    Token contract NatSpec annotations

--- Project/contracts/Token.sol
+++ Project/contracts/Token.sol
@@ -6,14 +6,23 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
+/// @title Voting token
+/// @author EncodeBootcamp
+/// @notice You can use this token to vote on ballot contract proposals
 contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
+    /// @notice Used for granting the minter role 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    /// @notice Token contract constructor
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    /// @notice Mint tokens
+    /// @dev Only minter role can execute this function
+    /// @param to Address the minted tokens will be sent to
+    /// @param amount Amount of tokens to be minted
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
